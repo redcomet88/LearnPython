c42706e72ea81704a3cf6eede1daf6949ac3e83e
redcomet88 <626206333@qq.com>
2018-11-30 19:51:28 +0800

    Learn Python3 the hard way

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is igored by python.
+
+print("I cound have code like this.") # and the comment after is ignored
+
+# You can also use comment to "disabel" or comment out code:
+# print("This won't run.")
+
+print("This will run.")
---
+++ ex3.py
@@ -0,0 +1,26 @@
+print("I will now count my chickens:")
+
+print("Hens", 25 + 30/6)
+print("Roosters", 100 - 25 * 3 % 4)
+# 看看这个优先级是怎么样子的 * 和 %
+# 增加多个0，也只会有一个0
+print("Roosters", 100.0000 - 25 % 3 * 4)
+
+print("Now I will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7 ?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("It is greater?", 5 > -2)
+print("It is greater or equal?", 5 >= -2)
+print("It is less or equal?", 5 <= -2)
